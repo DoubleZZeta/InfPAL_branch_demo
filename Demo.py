60aa9bf702f4ba9b14510a7c4d8d5f1adea54267
DoubleZZeta <zhangao_tony@163.com>
2025-02-05 19:24:27 +0000

    Update branch

--- Demo.py
+++ Demo.py
@@ -1,11 +1,15 @@
-def printnumbers(n):
-    for i in range n:
+def count_up(n):
+    for i in range(n):
+        print(i)
+
+def count_down(n);
+    for i in range(n-1,-1):
         print(i)
 
 
 
 
 
-
-
 def main():
+    count_up(10)
+    count_down(10)
